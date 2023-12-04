4fb5d2b347cc7b305019094f91f03670cb23de13
unknown <edgh81@gmail.com>
2023-12-04 16:46:29 -0400

    segundo

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ function App() {
   const [carrito, setCarrito] = useState([]);
   return (
     <>
-
       <BrowserRouter>
         <Header carrito={carrito} setCarrito={setCarrito}/>
         <Routes>
